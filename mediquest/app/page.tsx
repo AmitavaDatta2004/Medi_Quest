40f0c30257eb6a3bf93cb74b16fe6264c1f4e81c
Amitava <dattaarup2004@gmail.com>
2025-02-22 11:29:32 +0530

    page.tsx updated

---
+++ mediquest/app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import Developers from "@/components/Developers";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Testimonials from "@/components/Testimonials";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Testimonials />
+      <Developers />
+      <Footer />
+    </main>
+  );
+}
