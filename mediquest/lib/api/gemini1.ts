85e80cfcbdec0892f33c16389215bc6b65cd6147
Amitava <dattaarup2004@gmail.com>
2025-04-05 23:22:01 +0530

    gemini model changed

--- mediquest/lib/api/gemini1.ts
+++ mediquest/lib/api/gemini1.ts
@@ -19,7 +19,7 @@ export class GeminiService {
       throw new Error("Gemini API key is required");
     }
     this.genAI = new GoogleGenerativeAI(apiKey);
-    this.model = this.genAI.getGenerativeModel({ model: "gemini-2.5-pro-exp-03-25" }) as GenerativeModel;
+    this.model = this.genAI.getGenerativeModel({ model: "gemini-2.0-flash" }) as GenerativeModel;
   }
 
   async generateContent(prompt: string): Promise<MedicineData> {
